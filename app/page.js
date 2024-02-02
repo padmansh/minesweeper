970e394928aa1fdcf1f7ad1da7f9f1a4496d8f98
Aniket Padmansh <pudmaansh@gmail.com>
2024-02-03 02:44:33 +0530

    mvp

--- app/page.js
+++ app/page.js
@@ -1,113 +1,320 @@
+"use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import mine from "../public/mine.png";
+import pin from "../public/pin.png";
+
+const MINE_COUNT = 50;
+const BOARD_SIZE = 20;
+const TIMEOUT_TIME = 150;
+
+const textClasses = {
+  0: "",
+  1: "text-[#0000FF]",
+  2: "text-[#00A36C]",
+  3: "text-[#FF0000]",
+  4: "text-[#00008B]",
+  5: "text-[#964B00]",
+  6: "text-[#008B8B]",
+  7: "text-black",
+  8: "text-[#808080]",
+};
 
 export default function Home() {
+  const [board, setBoard] = useState([]);
+  const [mines, setMines] = useState([]);
+  const [visitedTiles, setVisitedTiles] = useState(0);
+  const [flagged, setFlagged] = useState(0);
+  const [gameStatus, setGameStatus] = useState("PLAY");
+
+  const randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const putMine = (board, mines, count) => {
+    if (count === MINE_COUNT) return { board, mines };
+    const row = randomIntFromInterval(0, BOARD_SIZE - 1);
+    const col = randomIntFromInterval(0, BOARD_SIZE - 1);
+
+    if (board[row][col].mine) {
+      return putMine(board, mines, count);
+    } else {
+      board[row][col].mine = true;
+      if (row - 1 >= 0 && col - 1 >= 0) {
+        board[row - 1][col - 1].count = board[row - 1][col - 1].count + 1;
+      }
+      if (row - 1 >= 0) {
+        board[row - 1][col].count = board[row - 1][col].count + 1;
+      }
+      if (row - 1 >= 0 && col + 1 <= BOARD_SIZE - 1) {
+        board[row - 1][col + 1].count = board[row - 1][col + 1].count + 1;
+      }
+      if (row + 1 <= BOARD_SIZE - 1 && col - 1 >= 0) {
+        board[row + 1][col - 1].count = board[row + 1][col - 1].count + 1;
+      }
+      if (row + 1 <= BOARD_SIZE - 1) {
+        board[row + 1][col].count = board[row + 1][col].count + 1;
+      }
+      if (row + 1 <= BOARD_SIZE - 1 && col + 1 <= BOARD_SIZE - 1) {
+        board[row + 1][col + 1].count = board[row + 1][col + 1].count + 1;
+      }
+      if (col + 1 <= BOARD_SIZE - 1) {
+        board[row][col + 1].count = board[row][col + 1].count + 1;
+      }
+      if (col - 1 >= 0) {
+        board[row][col - 1].count = board[row][col - 1].count + 1;
+      }
+      count++;
+      mines.push({ row, col });
+      return putMine(board, mines, count);
+    }
+  };
+
+  const createBoard = () => {
+    let tile = {
+      visited: false,
+      count: 0,
+      mine: false,
+      poc: false,
+      flagged: false,
+    };
+
+    const boardArr = Array(BOARD_SIZE).fill(
+      Array(BOARD_SIZE).fill({ ...tile })
+    );
+    const { board: boardArray, mines: mineArr } = putMine(
+      JSON.parse(JSON.stringify(boardArr)),
+      [],
+      0
+    );
+    setVisitedTiles(0);
+    setFlagged(0);
+    setGameStatus("PLAY");
+    setBoard(boardArray);
+    setMines(mineArr);
+  };
+
+  const recursiveVisit = (board, row, col) => {
+    if (
+      row < 0 ||
+      row > BOARD_SIZE - 1 ||
+      col < 0 ||
+      col > BOARD_SIZE - 1 ||
+      board[row][col].visited
+    ) {
+      return;
+    }
+
+    if (board[row][col].count > 0) {
+      board[row][col].visited = true;
+      setVisitedTiles((visitedTiles) => visitedTiles + 1);
+      return;
+    }
+
+    board[row][col].visited = true;
+    setVisitedTiles((visitedTiles) => visitedTiles + 1);
+    recursiveVisit(board, row - 1, col - 1);
+    recursiveVisit(board, row - 1, col);
+    recursiveVisit(board, row - 1, col + 1);
+    recursiveVisit(board, row + 1, col - 1);
+    recursiveVisit(board, row + 1, col);
+    recursiveVisit(board, row + 1, col + 1);
+    recursiveVisit(board, row, col + 1);
+    recursiveVisit(board, row, col - 1);
+  };
+
+  const playMineSound = () => {
+    const sound = new Audio("/mineBlast.mp3");
+    sound.volume = 1;
+    sound.play();
+  };
+
+  const blast = (mineCount, mine, remainingMines, TIMEOUT_TIME) => {
+    return new Promise((res) => {
+      if (board[mine.row][mine.col].visited) {
+        if (mineCount) {
+          blast(
+            mineCount - 1,
+            remainingMines[mineCount - 1],
+            remainingMines,
+            TIMEOUT_TIME - 3
+          );
+        }
+
+        res();
+        return;
+      }
+
+      setTimeout(async () => {
+        setBoard((board) => {
+          const boardDup = JSON.parse(JSON.stringify(board));
+          boardDup[mine.row][mine.col].visited = true;
+
+          return boardDup;
+        });
+
+        playMineSound();
+
+        if (mineCount) {
+          blast(
+            mineCount - 1,
+            remainingMines[mineCount - 1],
+            remainingMines,
+            TIMEOUT_TIME - 3
+          );
+        }
+
+        res();
+      }, TIMEOUT_TIME);
+    });
+  };
+
+  const blastMines = async (remainingMines, TIMEOUT_TIME) => {
+    const mineCount = remainingMines?.length;
+
+    await blast(
+      mineCount,
+      remainingMines[mineCount - 1],
+      remainingMines,
+      TIMEOUT_TIME
+    );
+  };
+
+  const handleTileVisit = (row, col) => {
+    const boardDup = JSON.parse(JSON.stringify(board));
+    if (boardDup[row][col].visited) return;
+    if (boardDup[row][col].mine) {
+      boardDup[row][col].visited = true;
+      boardDup[row][col].poc = true;
+
+      const mineIndex = mines?.findIndex(
+        (mine) => mine?.row === row && mine?.col === col
+      );
+      const remainingMines = [...mines];
+      remainingMines?.splice(mineIndex, 1);
+
+      playMineSound();
+
+      setBoard(boardDup);
+      setGameStatus("LOSE");
+      setTimeout(() => blastMines(remainingMines, TIMEOUT_TIME), TIMEOUT_TIME);
+      return;
+    }
+
+    if (!boardDup[row][col].visited) {
+      recursiveVisit(boardDup, row, col);
+      setBoard(boardDup);
+    }
+  };
+
+  const markFlag = (row, col) => {
+    const boardDup = JSON.parse(JSON.stringify(board));
+
+    if (boardDup[row][col].flagged) {
+      boardDup[row][col].flagged = false;
+      boardDup[row][col].visited = false;
+
+      setVisitedTiles((visited) => visited - 1);
+      setFlagged((flag) => flag - 1);
+    } else {
+      if (flagged <= MINE_COUNT && !boardDup[row][col].visited) {
+        boardDup[row][col].flagged = true;
+        boardDup[row][col].visited = true;
+        setFlagged((flag) => flag + 1);
+        setVisitedTiles((visited) => visited + 1);
+      }
+    }
+
+    setBoard(boardDup);
+  };
+
+  const tileClick = (e, row, col) => {
+    if (gameStatus !== "PLAY") return;
+    if (e?.which === 3 || e?.button === 2) {
+      markFlag(row, col);
+    } else {
+      handleTileVisit(row, col);
+    }
+  };
+
+  const winGame = () => {
+    if (visitedTiles + MINE_COUNT - flagged === BOARD_SIZE * BOARD_SIZE) {
+      setGameStatus("WIN");
+
+      const dupBoard = JSON.parse(JSON.stringify(board));
+      mines?.forEach((mine) => {
+        dupBoard[mine?.row][mine?.col].flagged = true;
+        dupBoard[mine?.row][mine?.col].visited = true;
+      });
+      setBoard(dupBoard);
+    }
+  };
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  useEffect(winGame, [visitedTiles, flagged]);
+
+  useEffect(() => {
+    const removeMenu = (event) => {
+      event.preventDefault();
+    };
+    document.addEventListener("contextmenu", removeMenu);
+
+    return () => document.removeEventListener("contextmenu", removeMenu);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div
+        className="cursor-pointer mb-4 px-8 py-1 rounded-md bg-gray-400 hover:bg-gray-300 duration-300"
+        onClick={() => {
+          createBoard();
+        }}
+      >
+        Reload
+      </div>
+
+      {board?.map((row, rowId) => (
+        <div key={rowId} className="flex">
+          {row?.map((col, colId) => (
+            <div
+              onMouseDown={(e) => tileClick(e, rowId, colId)}
+              key={colId}
+              className={`w-8 h-8 flex justify-center items-center border-black border-opacity-50 duration-200 cursor-pointer ${
+                col?.visited ? "bg-gray-300" : "white"
+              } ${textClasses?.[col?.count]} font-bold text-lg ${
+                colId === BOARD_SIZE - 1 ? "border-r-[1px]" : ""
+              } ${
+                rowId === BOARD_SIZE - 1 ? "border-b-[1px]" : ""
+              } border-l-[1px] border-t-[1px] ${
+                col?.poc ? "bg-[#FF0000] bg-opacity-75" : "hover:bg-gray-300"
+              } relative`}
+            >
+              {col?.visited ? (
+                col?.flagged ? (
+                  <>
+                    <Image src={pin} height={24} width={24} alt="pin" />
+                    {gameStatus === "LOSE" && !col?.mine ? (
+                      <div className="absolute items-center justify-center flex rotate-45">
+                        <div className="h-7 w-[3px] rounded-md bg-red-500 absolute" />
+                        <div className="w-7 h-[3px] rounded-md bg-red-500 absolute" />
+                      </div>
+                    ) : null}
+                  </>
+                ) : col?.mine ? (
+                  <Image src={mine} height={24} width={24} alt="mine" />
+                ) : (
+                  col?.count || ""
+                )
+              ) : (
+                ""
+              )}
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      ))}
     </main>
   );
 }
